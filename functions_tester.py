2c6c394cdcf2d07afce64d240775ff7a4bd07d4a
newmanhw <newmanhwaters@gmail.com>
2025-03-11 13:31:51 -0400

    added function tester code

---
+++ functions_tester.py
@@ -0,0 +1,16 @@
+import m8
+
+module = m8
+function_name = input()
+function = getattr(module, function_name)
+inputs = input()
+if len(inputs) > 0:
+    inputs = inputs.split("|")
+    inputs = map(str.strip, inputs)
+    inputs = map(eval, inputs)
+    inputs = list(inputs)
+else:
+    inputs = []
+return_value = function(*inputs)
+inputs = map(repr, inputs)
+print(f"{function_name}({', '.join(inputs)}) returned {repr(return_value)}")
